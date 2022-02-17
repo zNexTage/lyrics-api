9049bc6f87a95419937ac03a98acaf7409009a1a
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-02-16 22:18:38 -0300

    Removi o import do useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react';
 import VagalumeApi from './Api/VagalumeApi';
 import NormalizeLyrics from './DataLayer/NormalizeLyric';
 import VagalumeResponse from './DataLayer/VagalumeResponse';
